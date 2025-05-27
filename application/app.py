f169c6919fd919fcaba32c9d7cb5c7ac4f2726a8
Maccabee Levine <msl321@lehigh.edu>
2025-05-27 15:46:21 -0400

    Fix cookie path

--- application/app.py
+++ application/app.py
@@ -188,7 +188,7 @@ def login():
 def logout():
     session.clear()
     response = make_response("Logged out", 401)
-    response.delete_cookie("ldapAuth_session_token")
+    response.delete_cookie("ldapAuth_session_token", path=url_for("home"))
     return response
 
 
