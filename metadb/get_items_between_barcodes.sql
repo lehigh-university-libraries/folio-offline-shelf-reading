6235556d2f9458e20344a87ed859d1ad1a4d50a5
Maccabee Levine <msl321@lehigh.edu>
2026-03-27 16:40:44 -0400

    Revert last two changes

--- metadb/get_items_between_barcodes.sql
+++ metadb/get_items_between_barcodes.sql
@@ -40,7 +40,7 @@ WITH
 		LEFT JOIN folio_inventory.item item_raw
 			ON item.id = item_raw.id
 	)
-SELECT DISTINCT
+SELECT
 	item.barcode, 
 	item.id,
 	item.effective_shelving_order, 
@@ -74,7 +74,7 @@ AND COALESCE (item_notes.note, item.effective_shelving_order) <=
 	COLLATE ucs_basic
 AND (item.discovery_suppress IS NULL OR NOT item.discovery_suppress)
 AND item.barcode IS NOT NULL
-ORDER BY COALESCE (local_shelving_order, effective_shelving_order)
+ORDER BY COALESCE (item_notes.note, item.effective_shelving_order)
 	COLLATE ucs_basic
 $$
 LANGUAGE SQL;
